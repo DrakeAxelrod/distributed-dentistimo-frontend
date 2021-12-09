b1f779dd776a1a5347a28ccc8f1e31e0a2462eae
Drake Axelrod <drake@draxel.io>
2021-12-09 23:09:44 +0100

    Add ability to display available timeslots
    robinhan <robinhan@student.chalmers.se>
    
    frontend now gets all the available timeslots and displays them to the
    user

--- components/map/BookAppointmentForm.tsx
+++ components/map/BookAppointmentForm.tsx
@@ -11,6 +11,7 @@ import {
   useColorModeValue,
   Box,
   Text,
+  Select,
 } from "@chakra-ui/react";
 import { FC, useEffect, useState } from "react";
 import { useMqttState, useSubscription, IMessage } from "mqtt-react-hooks";
@@ -96,18 +97,31 @@ type Props = {
     };
   };
 };
+
+const parseTimeSlot = (appointment: any) => {
+  const startHour = appointment.time.start.hour;
+  const startMinute = appointment.time.start.minute;
+  const endHour = appointment.time.end.hour;
+  const endMinute = appointment.time.end.minute;
+  return `${startHour}:${startMinute === 0 ? "00" : startMinute}-${endHour}:${
+    endMinute === 0 ? "00" : endMinute
+  }`;
+};
 export const BookAppointmentForm: FC<Props> = (props) => {
   const { client } = useMqttState();
   const { message, connectionStatus } = useSubscription(
-    "frontend/bookings/all",
+    "frontend/bookings/available",
   );
   const color = useColorModeValue("teal.500", "teal.100");
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [appointments, setAppointments] = useState([]);
+  const [timeSlot, setTimeSlot] = useState("");
+  const [canBook, setCanBook] = useState(true);
   const future = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
   const onChange = (selectedItem: Date) => {
     client
       ? client.publish(
-          "bookings/all",
+          "bookings/available",
           JSON.stringify({
             clinic: props.clinic,
             date: {
@@ -121,21 +135,22 @@ export const BookAppointmentForm: FC<Props> = (props) => {
           }),
         )
       : null;
-    setTimeout(() => {
-      if (connectionStatus) {
-        if (message) {
-          const msg = message.message ? message.message : "{}";
-          const data = JSON.parse(msg as string);
-          console.log(data);
-          if (data === {}) {
-            return;
-          } else {
-            return;
-          }
-        }
-      }
-    }, 1000);
   };
+  const onTimeSelect = (event: any) => {
+    setTimeSlot(event.target.value);
+    setCanBook(false);
+  };
+  const confirmBooking = () => {
+    //book appointment
+  };
+  useEffect(() => {
+    if (message) {
+      if (message.message) {
+        const data = JSON.parse(message.message as string);
+        setAppointments(data);
+      }
+    }
+  }, [message]);
   return (
     <>
       <Global styles={style} />
@@ -157,15 +172,27 @@ export const BookAppointmentForm: FC<Props> = (props) => {
           <ModalBody>
             <Calendar
               activeStartDate={new Date()}
+              defaultActiveStartDate={new Date()}
               minDate={new Date()}
               maxDate={future}
               onChange={onChange}
             />
-            <Text>availble appointment</Text>
+            <Heading align="center" py="1rem">
+              Appointment Time
+            </Heading>
+            <Select colorScheme="teal" isRequired onChange={onTimeSelect}>
+              {appointments.map((appointment, index: number) => {
+                return (
+                  <option key={index} value={parseTimeSlot(appointment)}>
+                    {parseTimeSlot(appointment)}
+                  </option>
+                );
+              })}
+            </Select>
             <Button
-              isDisabled={store.getState()._id === ""}
+              isDisabled={canBook}
               my="1rem"
-              onClick={() => {}}
+              onClick={confirmBooking}
               colorScheme="teal"
               width="full"
               mt={4}>
